8afaec185a0a87db8b9c4f105134d1d60865297b
Nils Destoop <nils.destoop@gmail.com>
2017-06-19 14:21:59 +0200

    fix the application

--- src/app/layouts/full-width/full-width.component.ts
+++ src/app/layouts/full-width/full-width.component.ts
@@ -1,10 +1,23 @@
 import {Component} from '@angular/core';
 import {AbstractLayoutComponent} from '../../shared/components/abstract-layout.component';
+import {WidgetTypeRegistry} from "../../shared/services/widget-type-registry.service";
 
 @Component({
     'selector': 'app-full-width-layout',
     'templateUrl': './full-width.component.html'
 })
 export class FullWidthRowLayoutComponent extends AbstractLayoutComponent {
+
+    /**
+     * Construct the row preview.
+     *
+     * @param dragulaService
+     * @param _componentFactoryResolver
+     * @param registry
+     * @param widgetService
+     */
+    constructor(protected widgetTypeRegistry: WidgetTypeRegistry) {
+        super(widgetTypeRegistry);
+    }
 
 }
