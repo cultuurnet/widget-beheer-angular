a31c5d765c486356bd9706cee7bf75951217a01f
Wouter Vanelslander <wouter.vanelslander@wunderkraut.com>
2017-06-30 15:24:57 +0200

    feature/WID-18: Move "widget remove" responsibility from the layout components to the widget preview component

--- src/app/core/layout/components/abstract-layout.component.ts
+++ src/app/core/layout/components/abstract-layout.component.ts
@@ -1,6 +1,4 @@
 import { Input } from "@angular/core";
-import { WidgetBuilderService } from "../../../widget-builder/services/widget-builder.service";
-import { Widget } from "../../widget/widget";
 
 export abstract class AbstractLayoutComponent {
 
@@ -8,20 +6,5 @@ export abstract class AbstractLayoutComponent {
    * The Layout regions
    */
   @Input() regions: any;
-
-  /**
-   * The selected widget
-   */
-  public selectedWidget: Widget;
-
-  /**
-   * Construct the row preview.
-   * @param widgetBuilderService
-   */
-  constructor(protected widgetBuilderService: WidgetBuilderService) {
-    widgetBuilderService.widgetSelected$.subscribe(widget => {
-        this.selectedWidget = widget
-    });
-  }
 
 }
