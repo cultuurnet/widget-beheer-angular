8ffe484d67d36afe6d8575e29ea5b9125b7608d1
Nils Destoop <nils.destoop@gmail.com>
2017-08-08 17:02:27 +0200

    General: Fix the rendering

--- src/app/core/widget/services/widget.service.ts
+++ src/app/core/widget/services/widget.service.ts
@@ -48,9 +48,9 @@ export class WidgetService {
     };
 
     if (widgetId) {
-      requestOptions.params.set('render', widgetId);
+      requestOptions.params = requestOptions.params.set('render', widgetId);
     }
-    
+
     return this.http.put(environment.apiUrl + this.widgetApiPath + 'project/' + widgetPage.project_id + '/widget-page', widgetPage, requestOptions)
       .do<WidgetSaveResponse>(widgetSaveReponse => {
         // Invalidate the widgetPageList cache for the given project id
