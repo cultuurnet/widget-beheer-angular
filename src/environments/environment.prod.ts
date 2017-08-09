d43fdc122226ec06cedeff3f19c1f92f6b961664
Nils Destoop <nils.destoop@gmail.com>
2017-08-09 15:56:21 +0200

    Added acceptance configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://projectaanvraag-api.dev/',
+  apiUrl: 'http://widgetbeheer-api-test.uitdatabank.be/',
   widgetApi: {
     currentVersion: 3,
     embedUrl: {
-      current: 'http://tools.uitdatabank.be/widgets/layout/:page_id.js',
-      legacy: 'http://tools.uitdatabank.be/widgets/layout/v2/:page_id.js',
+      current: 'http://widgetbeheer-api-test.uitdatabank.be/widgets/layout/:page_id.js',
+      legacy: 'http://widgetbeheer-api-test.uitdatabank.be/widgets/layout/v2/:page_id.js',
     }
   },
   projectaanvraagDashboardUrl: 'https://projectaanvraag.uitdatabank.be/'
