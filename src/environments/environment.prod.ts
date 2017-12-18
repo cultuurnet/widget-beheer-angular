f7606b24992df6bc9fce4b58d93cd99e103dbaf9
Nils Destoop <nils.destoop@gmail.com>
2017-12-18 13:51:12 +0100

    Use the projectaanvraag endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://widgetbeheer-api-test.uitdatabank.be/',
+  apiUrl: 'https://projectaanvraag-api-test.uitdatabank.be/',
   widgetApi: {
     currentVersion: 3,
     embedUrl: {
-      current: 'http://widgetbeheer-api-test.uitdatabank.be/widgets/layout/:page_id.js',
-      force_current: 'http://widgetbeheer-api-test.uitdatabank.be/widgets/layout/v3/:page_id.js',
+      current: 'http://projectaanvraag-api-test.uitdatabank.be/widgets/layout/:page_id.js',
+      force_current: 'http://projectaanvraag-api-test.uitdatabank.be/widgets/layout/v3/:page_id.js',
     }
   },
   projectaanvraagDashboardUrl: 'https://projectaanvraag-test.uitdatabank.be/',
