f2cec55bfb9a3a1ba222b6cc1f4abd2dee0948d4
Nils Destoop <nils.destoop@gmail.com>
2017-12-21 12:18:36 +0100

    update the source environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://widgetbeheer-api.dev/',
+  apiUrl: 'http://projectaanvraag-api.dev/',
   widgetApi: {
     currentVersion: 3,
     embedUrl: {
-      current: 'http://widgetbeheer-api.dev/widgets/layout/:page_id.js',
-      force_current: 'http://widgetbeheer-api.dev/widgets/layout/v3/:page_id.js',
+      current: 'http://projectaanvraag-api.dev/widgets/layout/:page_id.js',
+      force_current: 'http://projectaanvraag-api.dev/widgets/layout/v3/:page_id.js',
     }
   },
   projectaanvraagDashboardUrl: 'http://projectaanvraag.dev/',
