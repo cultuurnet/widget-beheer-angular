7a1defad205bbded1a51638d9707b572379cb686
Reinout Bossuyt <reinout@cultuurnet.be>
2018-01-30 12:24:25 +0100

    WID-94 use right hashes

--- src/environments/environment.jenkins.ts
+++ src/environments/environment.jenkins.ts
@@ -1,9 +1,9 @@
 export const environment = {
-	"production":"ab8e18ef4ebebeddc0b3152ce9c9006e14fc05242e3fc9ce32246ea6a9543074",
-	"apiUrl":"19b4a7f2aff78b674ac7bf36caa779c1f9348da1d43a7dce0ca08c556aef35bd",
-	"widgetApi_currentVersion":"9a61dd8d8c83bb48874b76f333e05cee30091a8f5b92a83e4fea6a9908546866",
-	"widgetApi_embedUrl_current":"b580212b032fa4aabe64bc87268890f044e5726a12324bf2e8da515eebc38b23",
-	"widgetApi_embedUrl_forceCurrent":"52cbb9ee8446280e58479d7a4b2673b3b7cf26ca8a994a0d4eab283cd0325c16",
-	"projectaanvraagDashboardUrl":"1fb0fe05eab2cfc45e41f9fdfd5eaab917f03839693da1bad98d95fb39d10dc4",
-	"zendeskUrl":"f7df4024136061af5e4dd84985a288567b5f68d441a15d7c2f3ef2bd83665eea"
+  "production": "ab8e18ef4ebebeddc0b3152ce9c9006e14fc05242e3fc9ce32246ea6a9543074",
+  "apiUrl": "19b4a7f2aff78b674ac7bf36caa779c1f9348da1d43a7dce0ca08c556aef35bd",
+  "widgetApi_currentVersion": "028917f32663c98f1a4d9213dc7531022f5955c04e2e88a2c5a3cc03e760e1e4",
+  "widgetApi_embedUrl_current": "e0a87c68287d6e3fa60b4d8e83b90565509b935a40d4b2804eef5d155a25b021",
+  "widgetApi_forceCurrent": "ea66bbb025f81817fa6dd233bcb6f6171cb4d2603a215d72cf52973e487a30a2",
+  "projectaanvraagDashboardUrl": "1fb0fe05eab2cfc45e41f9fdfd5eaab917f03839693da1bad98d95fb39d10dc4",
+  "zendeskUrl": "f7df4024136061af5e4dd84985a288567b5f68d441a15d7c2f3ef2bd83665eea"
 };
