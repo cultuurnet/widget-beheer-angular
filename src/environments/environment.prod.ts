a37a12a3df53c540512e2919a8a8c1b794956dd8
Stijn Swaanen <swaanenstijn@gmail.com>
2018-04-10 12:32:17 +0200

    WID-118 change environment variable widgetApi_forceCurrent to widgetApi_embedUrl_forceCurrent for production environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'https://projectaanvraag-api-test.uitdatabank.be/',
   widgetApi_currentVersion: 3,
   widgetApi_embedUrl_current: 'http://projectaanvraag-api-test.uitdatabank.be/widgets/layout/:page_id.js',
-  widgetApi_forceCurrent: 'http://projectaanvraag-api-test.uitdatabank.be/widgets/layout/v3/:page_id.js',
+  widgetApi_embedUrl_forceCurrent: 'http://projectaanvraag-api-test.uitdatabank.be/widgets/layout/v3/:page_id.js',
   projectaanvraagDashboardUrl: 'https://projectaanvraag-test.uitdatabank.be/',
   zendeskUrl: 'http://www.zendesk.com'
 };
