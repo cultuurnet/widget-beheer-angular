ea0c4b1485f9504f81234b27bc240b6a4ce147d7
Reinout Bossuyt <reinout@cultuurnet.be>
2018-08-06 13:57:59 +0200

    WID-149

--- src/app/widget-builder/constants/ckeditor-config.ts
+++ src/app/widget-builder/constants/ckeditor-config.ts
@@ -22,6 +22,6 @@ export const ckeditorConfig = {
     removeButtons: 'Underline,Subscript,Superscript,Cut,Copy,PasteText,Paste,PasteFromWord,Scayt,Styles,Strike,RemoveFormat,Blockquote,Anchor,About,SpecialChar,HorizontalRule,Maximize',
     removeDialogFields: 'image:info:txtBorder;image:info:txtHSpace;image:info:txtVSpace;image:info:txtHSpace',
     image_previewText: ' ',
-    filebrowserUploadUrl: 'https://widgetbeheer.uitdatabank.dev/upload'
+    filebrowserUploadUrl: '/upload'
   }
 };
