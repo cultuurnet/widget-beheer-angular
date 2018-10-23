a7f0cd66db6ae191630704fe3679222f903bccab
reinoutbossuyt <reinout.bossuyt@cultuurnet.be>
2018-10-23 11:06:00 +0200

    Update environment.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,10 +3,10 @@
  */
 export const environment = {
   production: false,
-  apiUrl: 'http://projectaanvraag-api.dev/',
+  apiUrl: 'https://projectaanvraag-api.uitdatabank.dev/',
   widgetApi_currentVersion: 3,
-  widgetApi_embedUrl_current: 'http://projectaanvraag-api.dev/widgets/layout/:page_id.js',
-  widgetApi_embedUrl_forceCurrent: 'http://projectaanvraag-api.dev/widgets/layout/v3/:page_id.js',
-  projectaanvraagDashboardUrl: 'https://projectaanvraag.uitdatabank.be/',
+  widgetApi_embedUrl_current: 'https://projectaanvraag-api.uitdatabank.dev/widgets/layout/:page_id.js',
+  widgetApi_embedUrl_forceCurrent: 'https://projectaanvraag-api.uitdatabank.dev/widgets/layout/v3/:page_id.js',
+  projectaanvraagDashboardUrl: 'https://projectaanvraag.uitdatabank.dev/',
   zendeskUrl: 'http://www.zendesk.com'
 };
