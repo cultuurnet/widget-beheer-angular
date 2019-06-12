4ae5ad6a3d111f3bdfeb6ee47b57dca3cedaa8f8
brampauwelyn <brampauw@gmail.com>
2019-06-12 16:46:17 +0200

    Revert "WID-195 - implement line-break filter on filter query fields"
    This reverts commit c45eb15a0aa1714724d6d4be317a2475e06a1282.

--- src/app/widget-builder/components/widgets/group-filters/widget-group-filters-edit.component.ts
+++ src/app/widget-builder/components/widgets/group-filters/widget-group-filters-edit.component.ts
@@ -83,9 +83,6 @@ export class WidgetGroupFiltersEditComponent implements OnInit, OnDestroy {
         }
       }
 
-      // Sanitize groupFilters
-      this.groupFilters = this.sanitizeGroupFilters(this.groupFilters);
-
       // Notify watchers
       this.groupFiltersChanged.emit();
     });
@@ -239,22 +236,5 @@ export class WidgetGroupFiltersEditComponent implements OnInit, OnDestroy {
   public handleStatusUpdate(event: any) {
     this.groupFiltersChanged.emit();
   }
-
-  /**
-   * Sanitize groupFilters query filters (remove line breaks)
-   */
-
-   public sanitizeGroupFilters(filters: any){
-     var lineBreakRegex = new RegExp(/\r?\n|\r/g);
-     var filtersLength = filters.filters.length;
-     for(var i = 0; i < filtersLength; i++){
-       var filterOptionsLength = filters.filters[i].options.length;
-       for(var j = 0; j < filterOptionsLength; j++){
-         filters.filters[i].options[j].query = filters.filters[i].options[j].query.replace(lineBreakRegex,'');
-       }
-     }
-     return filters;
-   }
-
 
 }
