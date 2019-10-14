d64902addcb9174b71fc7689cad8c8de0026e8d6
brampauwelyn <brampauw@gmail.com>
2019-10-14 17:01:32 +0200

    III-3098 - get publisher string from config and transform it into array

--- src/app/widget-builder/components/widgets/search-results-widget/search-results-widget-edit.component.ts
+++ src/app/widget-builder/components/widgets/search-results-widget/search-results-widget-edit.component.ts
@@ -39,7 +39,7 @@ export class SearchResultsWidgetEditComponent extends BaseWidgetEditComponent {
   /**
    * Get the publishers from the environment config
    */
-  publishers: Array<string> = environment.publishers;
+  publishers: Array<string> = environment.publishers.split(',');
 
   selectedPublishers: Array<string> = [];
 
