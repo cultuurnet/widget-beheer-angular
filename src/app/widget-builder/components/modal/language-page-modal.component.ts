21cb8ce3f90b8150bf0fb5dea6c56d08e0eaaad5
brampauwelyn <brampauw@gmail.com>
2019-11-27 11:18:46 +0100

    add formbuilder to the language-page-modal component

--- src/app/widget-builder/components/modal/language-page-modal.component.ts
+++ src/app/widget-builder/components/modal/language-page-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { WidgetPage } from '../../../core/widget/widget-page';
 import { WidgetService } from '../../../core/widget/services/widget.service';
-
+import { FormBuilder, FormGroup} from '@angular/forms';
 /**
  * Confirmation modal component.
  */
@@ -12,6 +12,10 @@ import { WidgetService } from '../../../core/widget/services/widget.service';
 })
 export class LanguagePageModalComponent implements OnInit {
 
+  /**
+   * The language edit form
+   */
+  public languageForm: FormGroup;
 
   /**
    * Indicates if the widget is being saved
@@ -30,27 +34,36 @@ export class LanguagePageModalComponent implements OnInit {
    */
   public widgetPage: WidgetPage;
 
+  public language;
+
   /**
    * AdminPageModalComponent constructor.
    * @param activeModal
+   * @param formBuilder
    * @param widgetService
    */
   constructor(
     public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
     private widgetService: WidgetService
   ) { }
+
 
   /**
    * @inheritDoc
    */
   public ngOnInit() {
-
+    this.languageForm = this.formBuilder.group({
+      language: this.widgetPage.language
+    });
   }
 
   public save() {
     this.isSaving = true;
     this.publishing = true;
 
+    // Apply the language to the widgetpage model
+    this.widgetPage.language = this.languageForm.controls.language.value;
 
     // Save the widget page (will trigger a render for the current widget)
     this.widgetService.saveWidgetPage(this.widgetPage).subscribe(() => {
