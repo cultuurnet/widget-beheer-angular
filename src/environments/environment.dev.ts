779ecebf63d85f62066721e8fe06ffc70c74c947
bram <bram.pauwelyn@publiq.be>
2021-12-21 13:15:24 +0100

    WID-466 - Undo prettier formatting

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,14 +3,11 @@
  */
 export const environment = {
   production: false,
-  apiUrl: "https://projectaanvraag-api.uitdatabank.dev/",
+  apiUrl: 'https://projectaanvraag-api.uitdatabank.dev/',
   widgetApi_currentVersion: 3,
-  widgetApi_embedUrl_current:
-    "https://projectaanvraag-api.uitdatabank.dev/widgets/layout/:page_id.js",
-  widgetApi_embedUrl_forceCurrent:
-    "https://projectaanvraag-api.uitdatabank.dev/widgets/layout/v3/:page_id.js",
-  projectaanvraagDashboardUrl: "https://projectaanvraag.uitdatabank.dev/",
-  zendeskUrl: "http://www.zendesk.com",
-  publishers:
-    "BRUZZ,Gazet van Antwerpen,Indiestyle,UiTX,UiT,UiTMetVlieg,NXTPOP",
+  widgetApi_embedUrl_current: 'https://projectaanvraag-api.uitdatabank.dev/widgets/layout/:page_id.js',
+  widgetApi_embedUrl_forceCurrent: 'https://projectaanvraag-api.uitdatabank.dev/widgets/layout/v3/:page_id.js',
+  projectaanvraagDashboardUrl: 'https://projectaanvraag.uitdatabank.dev/',
+  zendeskUrl: 'http://www.zendesk.com',
+  publishers: 'BRUZZ,Gazet van Antwerpen,Indiestyle,UiTX,UiT,UiTMetVlieg,NXTPOP'
 };
