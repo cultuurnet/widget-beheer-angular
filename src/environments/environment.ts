aefd83eb9bdf1936c56aefad9a6464df5660a1c8
bram <bram.pauwelyn@publiq.be>
2021-12-21 13:10:12 +0100

    WID-466 - Add new publishers to env vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,14 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://projectaanvraag-api.uitdatabank.dev/',
+  apiUrl: "https://projectaanvraag-api.uitdatabank.dev/",
   widgetApi_currentVersion: 3,
-  widgetApi_embedUrl_current: 'http://projectaanvraag-api.uitdatabank.dev/widgets/layout/:page_id.js',
-  widgetApi_embedUrl_forceCurrent: 'https://projectaanvraag-api.uitdatabank.dev/widgets/layout/v3/:page_id.js',
-  projectaanvraagDashboardUrl: 'https://projectaanvraag.uitdatabank.dev/',
-  zendeskUrl: 'http://www.zendesk.com',
-  publishers: 'BRUZZ,Gazet van Antwerpen,Indiestyle,UiTX'
+  widgetApi_embedUrl_current:
+    "http://projectaanvraag-api.uitdatabank.dev/widgets/layout/:page_id.js",
+  widgetApi_embedUrl_forceCurrent:
+    "https://projectaanvraag-api.uitdatabank.dev/widgets/layout/v3/:page_id.js",
+  projectaanvraagDashboardUrl: "https://projectaanvraag.uitdatabank.dev/",
+  zendeskUrl: "http://www.zendesk.com",
+  publishers:
+    "BRUZZ,Gazet van Antwerpen,Indiestyle,UiTX,UiT,UiTMetVlieg,NXTPOP",
 };
