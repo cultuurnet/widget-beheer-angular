65996cea9cb588782fec736bebf914293429fddf
simon-debruijn <simon.debruijn@hotmail.com>
2022-05-13 15:55:05 +0200

    Change casing variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -25,14 +25,13 @@ export const setEnvironmentToConfig = async () => {
   const config = await getConfig();
 
   environment = {
-    production: config.NG_APP_PRODUCTION,
-    apiUrl: config.NG_APP_API_URL,
-    widgetApi_currentVersion: config.NG_APP_WIDGET_API_CURRENT_VERSION,
-    widgetApi_embedUrl_current: config.NG_APP_WIDGET_API_EMBED_URL_CURRENT,
-    widgetApi_embedUrl_forceCurrent:
-      config.NG_APP_WIDGET_API_EMBEDURL_FORCE_CURRENT,
-    projectaanvraagDashboardUrl: config.NG_APP_PROJECTAANVRAAG_DASHBOARD_URL,
-    zendeskUrl: config.NG_APP_ZENDESK_URL,
-    publishers: config.NG_APP_PUBLISHERS,
+    production: config.production,
+    apiUrl: config.apiUrl,
+    widgetApi_currentVersion: config.widgetApi_currentVersion,
+    widgetApi_embedUrl_current: config.widgetApi_embedUrl_current,
+    widgetApi_embedUrl_forceCurrent: config.widgetApi_embedUrl_forceCurrent,
+    projectaanvraagDashboardUrl: config.projectaanvraagDashboardUrl,
+    zendeskUrl: config.zendeskUrl,
+    publishers: config.publishers,
   };
 };
