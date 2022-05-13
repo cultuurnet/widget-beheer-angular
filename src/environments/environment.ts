719b41ddbbb856eddd05cb9185140dbc55b30dfe
simon-debruijn <simon.debruijn@hotmail.com>
2022-05-13 14:38:51 +0200

    Load config via request

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import config from '../assets/config.json';
+const getConfig = async () => await import('../assets/config.json');
 
 type Environment = {
   production: boolean;
@@ -13,14 +13,20 @@ type Environment = {
   publishers: string;
 };
 
-export const environment: Environment = {
-  production: config.NG_APP_PRODUCTION,
-  apiUrl: config.NG_APP_API_URL,
-  widgetApi_currentVersion: config.NG_APP_WIDGET_API_CURRENT_VERSION,
-  widgetApi_embedUrl_current: config.NG_APP_WIDGET_API_EMBED_URL_CURRENT,
-  widgetApi_embedUrl_forceCurrent:
-    config.NG_APP_WIDGET_API_EMBEDURL_FORCE_CURRENT,
-  projectaanvraagDashboardUrl: config.NG_APP_PROJECTAANVRAAG_DASHBOARD_URL,
-  zendeskUrl: config.NG_APP_ZENDESK_URL,
-  publishers: config.NG_APP_PUBLISHERS,
+export let environment: Environment;
+
+export const setEnvironmentFromConfig = async () => {
+  const config = await getConfig();
+
+  environment = {
+    production: config.NG_APP_PRODUCTION,
+    apiUrl: config.NG_APP_API_URL,
+    widgetApi_currentVersion: config.NG_APP_WIDGET_API_CURRENT_VERSION,
+    widgetApi_embedUrl_current: config.NG_APP_WIDGET_API_EMBED_URL_CURRENT,
+    widgetApi_embedUrl_forceCurrent:
+      config.NG_APP_WIDGET_API_EMBEDURL_FORCE_CURRENT,
+    projectaanvraagDashboardUrl: config.NG_APP_PROJECTAANVRAAG_DASHBOARD_URL,
+    zendeskUrl: config.NG_APP_ZENDESK_URL,
+    publishers: config.NG_APP_PUBLISHERS,
+  };
 };
