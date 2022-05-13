4ccca3e8a8c079e8350511728640b0acaf13cc6f
bram <bram.pauwelyn@publiq.be>
2022-05-13 11:39:04 +0200

    WID-454 - Use config.json from assets for environment vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import config from '../assets/config.json';
 
 export const environment = {
-  production: process.env.NG_APP_PRODUCTION,
-  apiUrl: process.env.NG_APP_API_URL,
-  widgetApi_currentVersion: process.env.NG_APP_WIDGET_API_CURRENT_VERSION,
-  widgetApi_embedUrl_current: process.env.NG_APP_WIDGET_API_EMBED_URL_CURRENT,
+  production: config.NG_APP_API_URL,
+  apiUrl: config.NG_APP_API_URL,
+  widgetApi_currentVersion: config.NG_APP_WIDGET_API_CURRENT_VERSION,
+  widgetApi_embedUrl_current: config.NG_APP_WIDGET_API_EMBED_URL_CURRENT,
   widgetApi_embedUrl_forceCurrent:
-    process.env.NG_APP_WIDGET_API_EMBEDURL_FORCE_CURRENT,
-  projectaanvraagDashboardUrl: process.env.NG_APP_PROJECTAANVRAAG_DASHBOARD_URL,
-  zendeskUrl: process.env.NG_APP_ZENDESK_URL,
-  publishers: process.env.NG_APP_PUBLISHERS,
+    config.NG_APP_WIDGET_API_EMBEDURL_FORCE_CURRENT,
+  projectaanvraagDashboardUrl: config.NG_APP_PROJECTAANVRAAG_DASHBOARD_URL,
+  zendeskUrl: config.NG_APP_ZENDESK_URL,
+  publishers: config.NG_APP_PUBLISHERS,
 };
