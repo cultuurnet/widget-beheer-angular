92b9addd9a88232aa59d7140b2b1bc6104f93bdc
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-11-29 14:46:19 +0100

    update name & redirect of BackButton depending on platformIsLive

--- src/app/widget-builder/components/page-list/page-list.component.ts
+++ src/app/widget-builder/components/page-list/page-list.component.ts
@@ -319,8 +319,8 @@ export class PageListComponent implements OnInit {
     this.topbarService.setBackButton(
       new BackButton(
         BackButton.TYPE_LINK,
-        'TOPBAR_BACK_BUTTON_LABEL_PROJECTAANVRAGEN',
-        environment.projectaanvraagDashboardUrl
+        environment.platformIsLive ? 'TOPBAR_BACK_BUTTON_LABEL_INTEGRATIES' : 'TOPBAR_BACK_BUTTON_LABEL_PROJECTAANVRAGEN',
+        environment.platformIsLive ? environment.platformUrl : environment.projectaanvraagDashboardUrl
       )
     );
   }
